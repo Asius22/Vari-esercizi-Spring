80fbfe6f1cd81bfbaa0e367787f386c43f6585b1
Asius22 <ciro_2000@live.it>
2022-04-22 16:48:16 +0200

    progetto creato

---
+++ ALGEBRA-LINEARE/src/main/java/com/corso/java/algebralineare/algebralineare/AlgebraLineareApplication.java
@@ -0,0 +1,13 @@
+package com.corso.java.algebralineare.algebralineare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgebraLineareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgebraLineareApplication.class, args);
+    }
+
+}
---
+++ ALGEBRA-LINEARE/src/test/java/com/corso/java/algebralineare/algebralineare/AlgebraLineareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corso.java.algebralineare.algebralineare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgebraLineareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
